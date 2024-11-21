6c6eb78a7b5968dc02096dae71efb3d80cd3a095
Hafsa_Rashid <hafsa20cse028@gmail.com>
2024-11-21 16:15:36 +0600

    update

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,29 +1,39 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model, connect } from 'mongoose'
 
-
-export type Guardian ={
-    
-        fatherName: string;
-        fatherOccupation: string;
-        fatherContactNo: string;
-        motherName: string;
-        motherOccupation: string;
-        motherContactNo: string;
-    
+export type Guardian = {
+  fatherName: string
+  fatherOccupation: string
+  fatherContactNo: string
+  motherName: string
+  motherOccupation: string
+  motherContactNo: string
 }
+
+export type UserName = {
+  firstName: string
+  lastName: string
+}
+
+export type LocalGuardian = {
+  name: string
+  occupation: string
+  contactNo: string
+  address: string
+}
+
 export type Student = {
-    id: string,
-    name: {
-        firstName: string;
-        lastName: string;
-    }
-    gender: "male" | "female";
-    dateOfBirth: string;
-    email: string;
-    contactNo: string;
-    emergencyContactNo: string;
-    bloodGroup? : "A+"|"A-"|"B+"|"B-"|"AB+"|"AB-"|"O+"|"O-";
-    presentAdd: string;
-    permanentAdd: string;
-    guardian: Guardian;
-  }
+  id: string
+  name: UserName
+  gender: 'male' | 'female'
+  dateOfBirth: string
+  email: string
+  contactNo: string
+  emergencyContactNo: string
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+  presentAdd: string
+  permanentAdd: string
+  guardian: Guardian
+  localGuardian: LocalGuardian
+  profileImg?: string
+  isActive: 'active' | 'inactive'
+}
