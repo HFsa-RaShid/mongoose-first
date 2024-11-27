5b8a7c8327681b08bc0801d7d2aab00a89d67e99
Hafsa_Rashid <hafsa20cse028@gmail.com>
2024-11-27 22:29:05 +0600

    update

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,50 +1,50 @@
 import { Request, Response } from 'express'
 import { StudentServices } from './student.service'
-import studentSchema from './student.zod.validation'
+// import studentSchema from './student.zod.validation'
 // import studentSchema from './student.validation';
 
-const createStudent = async (req: Request, res: Response) => {
-  try {
-    const { student: studentData } = req.body
+// const createStudent = async (req: Request, res: Response) => {
+//   try {
+//     const { student: studentData } = req.body
 
-    // data validation using zod
-    const zodParseData = studentSchema.parse(studentData)
-    const result = await StudentServices.createStudentIntoDB(zodParseData);
+//     // data validation using zod
+//     const zodParseData = studentSchema.parse(studentData)
+//     const result = await StudentServices.createStudentIntoDB(zodParseData);
 
-    // data validation using Joi
-    // const {error,value} = studentSchema.validate(studentData);
-    // const result = await StudentServices.createStudentIntoDB(value)
-    // console.log(error, value);
-    // if (error) {
-    //     res.status(500).json({
-    //       success: false,
-    //       message: 'Something went wrong',
-    //       error: error.details,
-    //     })
-    //   }
+//     // data validation using Joi
+//     // const {error,value} = studentSchema.validate(studentData);
+//     // const result = await StudentServices.createStudentIntoDB(value)
+//     // console.log(error, value);
+//     // if (error) {
+//     //     res.status(500).json({
+//     //       success: false,
+//     //       message: 'Something went wrong',
+//     //       error: error.details,
+//     //     })
+//     //   }
 
 
 
-    // will call service func to send this data(buildIn)
-    // const result = await StudentServices.createStudentIntoDB(studentData)
+//     // will call service func to send this data(buildIn)
+//     // const result = await StudentServices.createStudentIntoDB(studentData)
 
    
 
-      // Success Response
-      res.status(200).json({
-        success: true,
-        message: 'Student is created successfully',
-        data: result,
-      });
-    } catch (err: any) {
-      console.error("Error during student creation:", err.message); 
-      res.status(500).json({
-        success: false,
-        message: err.message || 'Something went wrong',
-        error: err,
-      });
-    }
-}
+//       // Success Response
+//       res.status(200).json({
+//         success: true,
+//         message: 'Student is created successfully',
+//         data: result,
+//       });
+//     } catch (err: any) {
+//       console.error("Error during student creation:", err.message); 
+//       res.status(500).json({
+//         success: false,
+//         message: err.message || 'Something went wrong',
+//         error: err,
+//       });
+//     }
+// }
 
 // get all student
 const getAllStudents = async (req: Request, res: Response) => {
@@ -110,7 +110,7 @@ const deleteStudent = async (req: Request, res: Response) => {
 }
 
 export const StudentControllers = {
-  createStudent,
+  // createStudent,
   getAllStudents,
   getSingleStudent,
   deleteStudent,
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,6 @@
-
-import { Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+import { Schema, model } from 'mongoose';
+import config from '../../config';
 import { TUser } from './user.interface';
 const userSchema = new Schema<TUser>(
   {
@@ -34,21 +35,21 @@ const userSchema = new Schema<TUser>(
   },
 );
 
-// userSchema.pre('save', async function (next) {
-//   // eslint-disable-next-line @typescript-eslint/no-this-alias
-//   const user = this; // doc
-//   // hashing password and save into DB
-//   user.password = await bcrypt.hash(
-//     user.password,
-//     Number(config.bcrypt_salt_rounds),
-//   );
-//   next();
-// });
+userSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this; // doc
+  // hashing password and save into DB
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
 
-// // set '' after saving password
-// userSchema.post('save', function (doc, next) {
-//   doc.password = '';
-//   next();
-// });
+// set '' after saving password
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
 
-// export const User = model<TUser>('User', userSchema);
+export const User = model<TUser>('User', userSchema);
